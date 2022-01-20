889a2cb6fddf571fd27b3b17a4a5b595d3272921
ExplerHD <73381264+ExplerHD@users.noreply.github.com>
2022-01-20 19:30:00 +0700

    Pls work

---
+++ src/OldEra/content/OldEraItems.java
@@ -0,0 +1,28 @@
+package oldera.content;
+
+import arc.graphics.Color;
+import mindustry.ctype.ContentList;
+import mindustry.type.Item;
+
+public static OldEraItems implements ContentList {
+	public static Item
+	
+	// itemList
+	metaglassShard, denseAlloy, diamondAlloy
+
+	@Override
+	public void load(){
+		metaglassShard = new Item("metaglass-shard", Color.valueOf("FFFFFF")){{
+			localizedName = "Metaglass Shards";
+			hardness = 1;
+		}};
+		denseAlloy = new Item("dense-alloy", Color.valueOf("CACACA")){{
+			localizedName = "Dense Alloy";
+			charge = 0.5;
+		}};
+		diamondAlloy = new Item("diamond-alloy", Color.valueOf("00ECFF")){{
+			localizedName = "Diamond Alloy";
+			charge = 10;
+		}};
+	}
+}
